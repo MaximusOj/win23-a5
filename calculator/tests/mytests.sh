a0ba0a3bd1fc64613c771783633dc9765e75e04a
MaximusOj <maxwellojiambo5@gmail.com>
2023-02-24 20:50:37 -0800

    added testfile into tests folder

---
+++ calculator/tests/mytests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+CALCULATOR=$1
+
+# Testing the multiplication function
+if ! $CALCULATOR 1 * 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+  echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
+  exit 1
+fi
+
+#Testing the subtraction function
+if ! $CALCULATOR 1 - 1; then  # If the return code of $PROGRAM is non-zero (i.e. error)...
+  echo 'ERROR! A valid run of the calculator (1 + 1) failed!'
+  exit 1
+fi
+
